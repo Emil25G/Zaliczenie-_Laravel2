37d9400d32690d31a03e258498811c62899dd009
SimonSQ <Gladki.szymon96@gmail.com>
2024-12-14 16:49:16 +0100

    Załatanie błędów

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -108,3 +108,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
